e071283223ef5f76eccac408ff3af0b0d89bf52b
Roman Vozbrannyi <romanvozbrannyi@gmail.com>
2020-10-23 18:39:44 +0300

    styled-components setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    
+    @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+    font-family: 'Roboto', sans-serif;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
+    <React.Fragment>
+      <GlobalStyle />
 
-    </div>
+    </React.Fragment>
   );
 }
 
